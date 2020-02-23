75c217bff4a2fb7408f01ed1f3e9b25e50652b92
alexandre.lloret <alloret.perso@gmail.com>
2020-02-23 00:19:08 +0000

    Add require check

---
+++ src/main/java/com/learning/defensivecoding/demo.java
@@ -0,0 +1,34 @@
+package com.learning.defensivecoding;
+
+import java.util.Objects;
+
+public class demo {
+    public static void main (String [] args){
+        System.out.println("Hello world");
+        firstCheck();
+    }
+
+    private static void firstCheck() {
+        SimpleClass simpleClass = null;
+        Objects.requireNonNull(simpleClass, "Cannot be null");
+
+    }
+
+    class SimpleClass{
+        private int val1;
+        private String val2;
+
+        public SimpleClass(int val1, String val2) {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+
+        public int getVal1() {
+            return val1;
+        }
+
+        public String getVal2() {
+            return val2;
+        }
+    }
+}
