4144a1a83f495b1c5da8f7bed1286e0d72bf8f14
alexandre.lloret <alloret.perso@gmail.com>
2020-03-09 20:35:32 +0000

    Comparable in a function

--- src/main/java/com/lambda/course1/Comparator.java
+++ src/main/java/com/lambda/course1/Comparator.java
@@ -6,7 +6,13 @@ import java.util.function.Function;
 public interface Comparator<T> {
     int compare(T t1, T t2);
 
+ /*   //default Comparator<T> thenComparing(Function<T, Comparable> function) {
+        return (p1,p2) -> function.apply()
+    }*/
+
     static Comparator<Person> comparing(Function<Person, String> f) {
-        return (p1, p2) -> f.apply(p1).compareTo(f.apply(p2)) ;
+        return (p1, p2) -> f.apply(p1).compareTo(f.apply(p2));
     }
+
+
 }
--- src/main/java/com/lambda/course1/demo.java
+++ src/main/java/com/lambda/course1/demo.java
@@ -1,6 +1,5 @@
 package com.lambda.course1;
 
-import java.util.Comparator;
 import java.util.function.Function;
 
 public class demo {
@@ -12,7 +11,7 @@ public class demo {
     };
     public static Comparator<String> lambda = (String o1, String o2) -> Integer.compare(o1.length(), o2.length());
 
-    public static Comparator<String> lambdaRefactored = Comparator.comparingInt(String::length);
+    //public static Comparator<String> lambdaRefactored = Comparator.comparing(String::length);
 
     public static Runnable r = new Runnable() {
         @Override
@@ -33,17 +32,20 @@ public class demo {
 
 
     private static Function<Person, Integer> getAge = Person::getAge;
-    private static Function<Person,String> getLastName = Person::getLastName;
+    private static Function<Person, String> getLastName = Person::getLastName;
     private static Comparator<Person> comparator = Comparator.comparing(getLastName);
 
     public static void main(String[] args) {
         //rLambda.run();
 
-        Person person2 = new Person(17,"xx","aa");
-        Person person1 = new Person(19,"ttdd","tt");
+        Person person2 = new Person(17, "xx", "aa");
+        Person person1 = new Person(19, "ttdd", "tt");
         System.out.println(getAge.apply(person1));
-        System.out.println(comparator.compare(person1,person2));
+        System.out.println(comparator.compare(person1, person2));
 
-
+       /* Comparator<Person> cmp = Comparator.comparing(Person::getLastName)
+                .thenComparing(Person::getFirstName)
+                .thenComparing(Person::getAge);*/
+       var test =1 ;
     }
 }
