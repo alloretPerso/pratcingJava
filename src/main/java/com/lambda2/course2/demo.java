03f394a52d6d58fee9a21bdb9e4ad3c65c0fb535
alexandre.lloret <alloret.perso@gmail.com>
2020-03-15 16:26:16 +0000

    Lambda2 course 2 extract word and select the one with size 4

--- src/main/java/com/lambda2/course2/demo.java
+++ src/main/java/com/lambda2/course2/demo.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class demo {
     public static void main(String[] args) {
-        try(Stream<String> stream1= Files.lines(Paths.get("files/page1.txt"));
-            Stream<String> stream2= Files.lines(Paths.get("files/page2.txt"))) {
+        try (Stream<String> stream1 = Files.lines(Paths.get("files/page1.txt"));
+             Stream<String> stream2 = Files.lines(Paths.get("files/page2.txt"))) {
            /* System.out.println("Stream 1 : " + stream1.count());
             System.out.println("Stream 2 : " + stream2.count());*/
 
@@ -18,6 +19,16 @@ public class demo {
 
 
             //System.out.println("#lines " + streamOfLines.count());
+
+            Function<String, Stream<String>> lineSplitter = line -> Pattern.compile(" ").splitAsStream(line);
+
+            Stream<String> streamOfWords = streamOfLines.flatMap(lineSplitter)
+                    .map(String::toLowerCase)
+                    .filter(word -> word.length() == 4)
+                    .distinct();
+
+            System.out.println("# words:" + streamOfWords.count());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
