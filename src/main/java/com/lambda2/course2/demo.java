16cc1c614cc6653b0e3decad056702569bdc3b42
alexandre.lloret <alloret.perso@gmail.com>
2020-03-15 21:41:04 +0000

    Lambda2 Play with sum max an statistics

--- src/main/java/com/lambda2/course2/demo.java
+++ src/main/java/com/lambda2/course2/demo.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,6 +16,9 @@ import static java.nio.file.Paths.*;
 import static java.util.stream.Collectors.*;
 
 public class demo {
+
+    public static final int[] SCRABBLE_EN_SCORE = new int[]{1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
     public static void main(String[] args) {
         try (Stream<String> stream1 = Files.lines(get("files/page1.txt"));
              Stream<String> stream2 = Files.lines(get("files/page2.txt"))) {
@@ -66,8 +70,7 @@ public class demo {
         }
     }
 
-    public static void playWithWords() {
-
+    private static void playWithWords() {
         try {
             Set<String> ospd = Files.lines(get("files/course2/ospd.txt"))
                     .map(String::toLowerCase)
@@ -78,8 +81,47 @@ public class demo {
 
             System.out.println("# Ospd number of words " + ospd.size());
             System.out.println("# Shakespeare number of words " + shakespeare.size());
+
+
+            Function<String, Integer> score = word -> word.chars().map(letter -> SCRABBLE_EN_SCORE[letter - 'a']).sum();
+
+            ToIntFunction<String> intScore = word -> word.chars().map(letter -> SCRABBLE_EN_SCORE[letter - 'a']).sum(); // will give the primitive int
+
+            System.out.println("Score of hello : " + intScore.applyAsInt("hello"));
+
+/*            String s = shakespeare.stream()
+                    .max(Comparator.comparing(score))
+                    .get();
+
+            String s1 = ospd.stream()
+                    .max(Comparator.comparing(score))
+                    .get();
+
+            System.out.println("The max word for Shakespeare is : " + s);
+            System.out.println("The max word for Ospd is : " + s1);*/
+
+
+            // Then we can use filter to reduce to only the word in ospd
+/*            String sFiltered = shakespeare.stream()
+                    .filter(word -> ospd.contains(word))
+                    .max(Comparator.comparing(score))
+                    .get();*/
+
+/*            System.out.println("The max word for Shakespeare after filter is : " + sFiltered);
+            System.out.println("Score of max word : "+ intScore.applyAsInt(sFiltered));*/
+
+            IntSummaryStatistics intSummaryStatistics = shakespeare.stream()
+                    .filter(ospd::contains)
+                    .mapToInt(intScore)
+                    .summaryStatistics();
+            System.out.println("Stats : " + intSummaryStatistics);
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 }
