df5a795f0dea5b7f929107d464021f4aff6aa0a5
alexandre.lloret <alloret.perso@gmail.com>
2020-03-22 21:29:26 +0100

    BUild a simple age comparator

---
+++ src/main/java/com/generics/interf/Person.java
@@ -0,0 +1,47 @@
+package com.generics.interf;
+
+import java.util.Objects;
+
+public final class Person {
+
+    private final String name;
+    private final int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(String name, int age) {
+        Objects.requireNonNull(name);
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/generics/interf/demo.java
@@ -0,0 +1,21 @@
+package com.generics.interf;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class demo {
+    public static void main(String[] args) {
+        Person donDraper = new Person("Don Draper", 89);
+        Person peggyOlson = new Person("Peggy Olson", 65);
+        Person bertCooper = new Person("Bert Cooper", 100);
+
+        List<Person> madMen = new ArrayList<>();
+        madMen.addAll(Arrays.asList(donDraper, peggyOlson, bertCooper));
+        System.out.println(madMen);
+        //Collections.sort(madMen, new AgeComparator());
+        System.out.println(madMen);
+    }
+}
