db2e89a692c4d0acaa8d63bfe60a638c159c075a
alexandre.lloret <alloret.perso@gmail.com>
2021-01-16 19:43:43 +0000

    Use of reflection

--- src/main/java/com/annotation/Main.java
+++ src/main/java/com/annotation/Main.java
@@ -1,15 +1,31 @@
 package com.annotation;
 
+import java.lang.reflect.Method;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         String keyWord = "add";
-        MathProcessing[] mathProcessing = {new Add(), new Sub()};
-        for (MathProcessing mathProcessing1 : mathProcessing) {
+        Object[] mathProcessing = {new Add(), new Sub()};
+        for (Object mathProcessing1 : mathProcessing) {
             CommandKeyword annotation = mathProcessing1.getClass().getAnnotation(CommandKeyword.class);
             String name = annotation.name();
             if (keyWord.equalsIgnoreCase(name)) {
                 System.out.println("This is a " + name);
             }
         }
+    }
+
+    private static double handleCalculation(Object processor, double leftVal, double rightVal) {
+        double result = 0d;
+        try{
+          CommandKeyword commandKeyword = processor.getClass().getAnnotation(CommandKeyword.class);
+          String methodName = commandKeyword.method();
+            Method processingMethod = processor.getClass().getMethod(methodName,double.class,double.class);
+            result = (double) processingMethod.invoke(processor,leftVal,rightVal);
+        } catch (Exception ex){
+            System.out.println("Error accessing method - " + ex.toString());
+        }
+        return result;
     }
 }
